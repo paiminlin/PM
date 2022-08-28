9f2594745571feeb3a9dda6b219d9d089ad6855e
git <2217561306@qq.com>
2022-08-29 07:14:20 +0800

    Update fsm

--- COMMON/fsm/test2.c
+++ COMMON/fsm/test2.c
@@ -1,102 +1,91 @@
-/*
- * http://zh.wikipedia.org/wiki/自动机编程
- */
 
 #include "fsm.h"
 #include <ctype.h>
 
 enum states {
-	before = 0,
-	inside = 1,
-	after  = 2,
+    OFF = 0,
+    ACC = 1,
+    ON  = 2,
 };
 
 enum event {
-	S = 0,	/* 空白字符 */
-	N = 1,	/* 换行字符 */
-	A = 2,	/* 其他字符 */
-	Q = 3,	/* EOF */
+    SSB     = 0,
+    STOP    = 1,
+    INVALID = 2,
 };
 
 struct para {
-	struct fsm_t *fsm;
-	int c;
+    struct fsm_t *fsm;
+    int c;
 };
 
-static void *stop(void *p)
+static int Get_SSBStatus(void *p)
 {
-	fsm_stop(((struct para *)p)->fsm);
-	return NULL;
+    ((struct para *)p)->c = getchar();
+    if (((struct para *)p)->c == 'S')
+        return SSB;
+    else if (((struct para *)p)->c == 'Q')
+        return STOP;
+    else
+        return INVALID;
 }
 
-static int get_char(void *p)
+static void *ToOFF(void *p)
 {
-	((struct para *)p)->c = getchar();
-	if (((struct para *)p)->c == '\n')
-		return N;
-	else if (isblank(((struct para *)p)->c))
-		return S;
-	else if (((struct para *)p)->c == EOF)
-		return Q;
-	else
-		return A;
+    printf("%s-%d\n",__func__, __LINE__);
+    return NULL;
 }
-
-static void *print(void *p)
+static void *ToACC(void *p)
 {
-	putchar(((struct para *)p)->c);
-	return NULL;
+    printf("%s-%d\n",__func__, __LINE__);
+    return NULL;
+}
+static void *ToON(void *p)
+{
+    printf("%s-%d\n",__func__, __LINE__);
+    return NULL;
+}
+static void *ToStop(void *p)
+{
+    printf("%s-%d\n",__func__, __LINE__);
+    fsm_stop(((struct para *)p)->fsm);
+    return NULL;
 }
 #if 0
-int main(int argc, char **argv)
+int main()
 {
-	struct para func_para;
-	struct fsm_branch branch[][4] = {
-		/* before */
-		{
-			/* S */
-			{S, before, NULL,},
-			/* N */
-			{N, before, print,},
-			/* A */
-			{A, inside, print,},
-			/* Q */
-			{Q, 0, stop,},
-		},
-		/* inside */
-		{
-			/* S */
-			{S, after, NULL,},
-			/* N */
-			{N, before, print,},
-			/* A */
-			{A, inside, print,},
-			/* Q */
-			{Q, 0, stop,},
-		},
-		/* after */
-		{
-			/* S */
-			{S, after, NULL,},
-			/* N */
-			{N, before, print,},
-			/* A */
-			{A, after, NULL,},
-			/* Q */
-			{Q, 0, stop,},
-		},
-	};
-	struct fsm_state fsm_state[] = {
-		{before, sizeof(branch[0]) / sizeof(branch[0][0]), branch[0],},
-		{inside, sizeof(branch[1]) / sizeof(branch[1][0]), branch[1],},
-		{after,  sizeof(branch[2]) / sizeof(branch[2][0]), branch[2],},
-	};
-	func_para.fsm = fsm_create_with_state(fsm_state, 
-				sizeof(fsm_state) / sizeof(fsm_state[0]), 
-				sizeof(branch[0]) / sizeof(branch[0][0]),
-				before);
-	fsm_run(func_para.fsm, get_char, &func_para, &func_para, NULL);
-	fsm_release(func_para.fsm);
-	return 0;
+    struct para func_para;
+    struct fsm_branch branch[][2] = {
+        /* OFF */
+        {
+            /* SSB */
+            {SSB, ACC, ToACC,},
+            {STOP, OFF, ToStop,},
+        },
+        /* ACC */
+        {
+            /* SSB */
+            {SSB, ON, ToON,},
+            {STOP, ACC, NULL,},
+        },
+        /* ON */
+        {
+            /* SSB */
+            {SSB, OFF, ToOFF,},
+            {STOP, ON, NULL,},
+        },
+    };
+    struct fsm_state fsm_state[] = {
+        {OFF, sizeof(branch[0]) / sizeof(branch[0][0]), branch[0],},
+        {ACC, sizeof(branch[1]) / sizeof(branch[1][0]), branch[1],},
+        {ON,  sizeof(branch[2]) / sizeof(branch[2][0]), branch[2],},
+    };
+    func_para.fsm = fsm_create_with_state(fsm_state, 
+                sizeof(fsm_state) / sizeof(fsm_state[0]), 
+                sizeof(branch[0]) / sizeof(branch[0][0]),
+                OFF);
+    fsm_run(func_para.fsm, Get_SSBStatus, &func_para, &func_para, NULL);
+    fsm_release(func_para.fsm);
+    return 0;
 }
 #endif
